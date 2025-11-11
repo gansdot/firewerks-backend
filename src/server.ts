5a8abf8c20333990043f57a623ab204dac9d7809
Ganesan Mariappan <gansdot@yahoo.com>
2025-11-11 20:33:06 +0100

    fix for frontend static content path

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,7 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+const uploadPath = express.static(path.join(__dirname, "../uploads"));
 
 app.get("/", (_, res) => res.send("🧨 E-commerce backend running"));
 
@@ -39,12 +40,13 @@ app.use("/api/upload", uploadRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/contact", contactRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "/home/ubuntu/firewerks/backend/uploads")));
-console.log("server.ts dirname :: ", __dirname);
+app.use("/uploads", uploadPath);
+console.log("server.ts uploadPath dirname :: ", uploadPath);
 
 // Serve static frontend files from the "dist" folder
 const frontendPath = path.resolve(__dirname, "/home/ubuntu/firewerks/frontend/dist");
 app.use(express.static(frontendPath));
+console.log("server.ts dirname frontendPath :: ", frontendPath);
 
 // Catch-all route to serve index.html for SPA routes
 app.get(/(.*)/, (req, res) => {
