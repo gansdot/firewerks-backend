baac3bdddcb107b71727db3e999840aad56524f4
Ganesan Mariappan <gansdot@yahoo.com>
2025-11-11 14:50:41 +0100

    issue regards to upload

--- dist/routes/upload.routes.js
+++ dist/routes/upload.routes.js
@@ -4,27 +4,28 @@ import path from "path";
 import fs from "fs";
 const router = express.Router();
 // ensure uploads folder exists
-const uploadDir = path.join(process.cwd(), "uploads");
-if (!fs.existsSync(uploadDir))
-    fs.mkdirSync(uploadDir);
+//const uploadDir = path.join(process.cwd(), "uploads");
+const uploadDir = path.join(__dirname, "../../uploads");
+
+if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 // configure multer for local disk storage
 const storage = multer.diskStorage({
-    destination: function (_req, _file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (_req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    },
+  destination: function (_req, _file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (_req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
 });
 const upload = multer({ storage });
 // POST /api/upload
 router.post("/", upload.single("image"), (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ message: "No file uploaded" });
-    }
-    // return the relative file path
-    const filePath = `/uploads/${req.file.filename}`;
-    res.json({ filePath });
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  // return the relative file path
+  const filePath = `/uploads/${req.file.filename}`;
+  res.json({ filePath });
 });
 export default router;
