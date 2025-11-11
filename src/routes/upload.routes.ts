73fb80d4473c97e36ebc336055ec817aa44cc570
Ganesan Mariappan <gansdot@yahoo.com>
2025-11-11 16:15:40 +0100

    fix for file upload

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -14,7 +14,8 @@ console.log("upload directory nothing .... ", process.cwd());
 console.log("upload directory __filename .... ", __filename);
 
 
-const uploadDir = path.join(process.cwd(), "../../uploads");
+const uploadDir = path.join(process.cwd(), "/backend/uploads");
+//const uploadDir = path.join(process.cwd(), "uploads");
 
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
