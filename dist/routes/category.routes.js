7583e2f48ef7c009c5ddd84e1b9e2c659e883c7f
Ganesan Mariappan <gansdot@yahoo.com>
2025-11-12 23:27:26 +0100

    Fix: stop tracking ignored files

--- dist/routes/category.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/categoryRoutes.js
-import express from "express";
-import { addCategory, getCategories } from "../controllers/category.controller.js";
-import { auth, admin } from "../middleware/auth.js";
-const router = express.Router();
-router.route("/")
-    .get(getCategories)
-    .post(auth, admin, addCategory);
-export default router;
--- dist/routes/reviews.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { addReview, getReviewsByProduct, getReviewSummary } from "../controllers/reviews.controller.js";
-const router = express.Router();
-router.post("/", addReview);
-router.get("/:productId", getReviewsByProduct);
-router.get("/summary/:productId", getReviewSummary);
-export default router;
