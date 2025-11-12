7349fb70411294a7b32b8b751cf770283b9c2d6a
Ganesan Mariappan <gansdot@yahoo.com>
2025-11-12 16:57:01 +0100

    fix upload, registraion, checkout

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -6,22 +6,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const router = express.Router();
-
-// ensure uploads folder exists
-//const uploadDir = path.join(process.cwd(), "uploads");
 const uploadConfig = process.env.UPLOAD_DIR!;
+const uploadDir = path.join(process.cwd(), uploadConfig!);
 
-
-const uploadDir = path.join(process.cwd(), uploadConfig);
-//const uploadDir = path.join(process.cwd(), "uploads");
-
-
-if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir!);
 
 // configure multer for local disk storage
 const storage = multer.diskStorage({
   destination: function (_req, _file, cb) {
-    cb(null, uploadConfig);
+    cb(null, uploadConfig!);
   },
   filename: function (_req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
